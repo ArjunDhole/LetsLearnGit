680298f6b996f694162fd5c9231efa1439e90ff4
ArjunDhole <amdhole02@gmail.com>
2024-08-17 15:06:15 +0530

    division file saved

---
+++ division.java
@@ -0,0 +1,9 @@
+public class division {
+    public static void main(String[] args) {
+        int a=40;
+        int b=20;
+        int div=a/b;
+        System.out.println(div);
+        
+    }
+}
